0ac53435252a045967695f55529c2c853e02e533
Reiji Reizan <matsurinoyama@pm.me>
2021-09-05 19:37:21 +0800

    no commot

--- Source/Server/DiB_USER.sol
+++ Source/Server/DiB_USER.sol
@@ -2,23 +2,22 @@
 pragma solidity ^0.5.0;
 
 contract DiB_USER {
-    
     struct User {
-        
         address id;
         string name;
         string dname;
         bool exist;
-        
     }
-    
+
     mapping(string => User) public users;
-    
-    function createUser(string memory _userName, string memory _userDname) public {
+
+    function createUser(string memory _userName, string memory _userDname)
+        public
+    {
         User storage user = users[_userName];
-        
+
         require(!user.exist, "ERROR: User already exists!");
-        
+
         users[_userName] = User({
             id: msg.sender,
             name: _userName,
